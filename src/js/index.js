b6f044824476545ddaeca7dfd224d42fb946ee2f
Pedro <pedro@MacBook-Pro-de-Pedro.local>
2024-02-16 00:27:58 -0300

    last commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,46 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
+// import FontAwesomeIcon  from '@fortawesome/react-fontawesome';
+// import faClock  from '@fortawesome/free-regular-svg-icons';
 
-//include bootstrap npm library into the bundle
-import "bootstrap";
 
-//include your index.scss file into the bundle
-import "../styles/index.scss";
+// include your styles into the webpack bundle
+import "../styles/index.css";
 
-//import your own components
-import { Home } from "./component/home.js";
+function SimpleCounter (props){
+    return (<div className="bigCounter">
+        <div className="calendar">
+            <i className="fa-regular fa-clock"></i>
+        </div>
+        <div className="four">{props.digitFour % 10}</div>
+        <div className="three">{props.digitThree % 10}</div>
+        <div className="two">{props.digitTwo % 10}</div>
+        <div className="one">{props.digitOne % 10}</div>
+    </div>);
+}
 
-//Componente para contador
-const SecondsCounter = props => {
-	return (
-		<div className="cronometro text-center bg-dark p-3">
-			<span className="indice">
-				<i className="fas fa-clock"></i>
-			</span>
-			<span className="cuatro">{props.indice4}</span>
-			<span className="tres">{props.indice3}</span>
-			<span className="dos">{props.indice2}</span>
-			<span className="uno">{props.indice1}</span>
-		</div>
-	);
+SimpleCounter.propTypes = { 
+    
+    digitFour : PropTypes.number, 
+    digitThree : PropTypes.number,
+    digitTwo : PropTypes.number,
+    digitOne : PropTypes.number
 };
-//
-//Protypes de los indices del contador
-SecondsCounter.propTypes = {
-	indice1: PropTypes.number,
-	indice2: PropTypes.number,
-	indice3: PropTypes.number,
-	indice4: PropTypes.number
-};
-//Inicializacion del contador
-let contador = 0;
-//usamos la funcion setinterval y metemos el render dentro para que renderise cada actualizacion
+
+let counter = 0;
+setInterval(function(){
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    console.log (four,three,two,one)
 //render your react application
-setInterval(function() {
-	//variables de cada numero del indice
-	const num1 = contador % 10;
-	const num2 = Math.floor(contador / 10) % 10;
-	const num3 = Math.floor(contador / 100) % 10;
-	const num4 = Math.floor(contador / 1000) % 10;
-	contador++;
-	//console.log("recargando cada segundo");
-	ReactDOM.render(
-		<SecondsCounter
-			indice1={num1}
-			indice2={num2}
-			indice3={num3}
-			indice4={num4}
-		/>,
-		document.querySelector("#app")
-	);
-}, 1000);
+    counter++;
+
+    ReactDOM.render(
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, 
+    document.querySelector("#app")
+);
+},1000);
